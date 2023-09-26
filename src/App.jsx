ca4e01fee9d7d6fc78083afa471c1bc33d0d94f8
Joel <ajiolajoel@gmail.com>
2023-09-26 18:49:46 +0100

    portfolio, under develpment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Projects from './components/Projects'
+import Testimonial from './components/Testimonial'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
-    <div><Home/></div>
+    <div><BrowserRouter>
+    <Routes>
+      <Route path='/' element ={<Home/>} />
+      <Route path ='/about' element={<About/>} />
+      <Route path='/services' element={<Services/>} />
+      <Route path='/projects' element={<Projects/>} />
+      <Route path='/testimonial' element={<Testimonial/>} />
+      <Route path='/contact' element={<Contact/>} />
+      </Routes>
+    </BrowserRouter>
+    </div>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Testimonial from './Testimonial.jsx'
 import Contact from './Contact.jsx'
 import About from './About'
 import Projects from './Projects.jsx'
 import Services from './Services.jsx'
-import myImage from "../assets/FB_IMG_1632361671640__2_-removebg-preview.png"
+import myImage from "../assets/FB_IMG_1632361671640 (2).jpg"
 
 
 const Home = () => {
@@ -12,13 +13,20 @@ const Home = () => {
     <div>
         {/* sidenav */}
         <div className='flex'>
-          <nav className='flex flex-col bg-black p-3'>
-            <img src={myImage} alt="image" className='w-98 h-96 rounded-full'/>
-            <ul className='flex flex-col'>
-                <li></li>
+          <nav className='flex flex-col bg-black p-5'>
+            
+            <img src={myImage} alt="image" className=' border rounded-full w-[60%] h-[100px] m-auto'/>
+            
+            <ul className='flex flex-col gap-6 p-10'>
+                <li className='text-white text-base text-center'><Link to="/">Home</Link></li>
+                <li className='text-white text-base text-center'><Link to="about">About Me</Link></li>
+                <li className='text-white text-base text-center'><Link to="services">What I Do</Link></li>
+                <li className='text-white text-base text-center'><Link to="projects">Projects</Link></li>
+                <li className='text-white text-base text-center'><Link to="testimonial">What Our Client Says</Link></li>
+                <li className='text-white text-base text-center'><Link to="contact">Contact Me</Link></li>
             </ul>
           </nav>
-          <div className=' bg-slate-500'>
+          <div className=' bg-slate-500 w-full'>
              <h3>hello</h3>
           </div>
         </div>
