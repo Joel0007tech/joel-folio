71a4783f76669f66abe26c7a8e09dc287f635eba
Joel <ajiolajoel@gmail.com>
2023-09-28 10:33:13 +0100

    update to my portfolio

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,15 +9,20 @@ const Services = () => {
       <h1 className='text-[150px] text-slate-200 text-center font-semibold'>SERVICES</h1>
         <h3 className='font-semibold text-4xl text-center relative bottom-32'>What I Do?</h3>
         <div className='flex justify-center gap-10 relative bottom-12'>
-        <div className='flex flex-col gap-2'>
-          <CgWebsite className='text-xl fill-green-600 text-gray-600'/>
+        <div className='flex flex-col gap-4 border-[3px] rounded-xl border-gray-500 p-7'>
+          <CgWebsite className='text-3xl fill-green-600 text-gray-600'/>
          <h3 className='font-bold text-xl '>Website Development</h3>
-         <p></p>
+         <p className='text-md font-normal text-slate-600 leading-relaxed'>I develop websites, for personal, business, or <br/>
+         organization useage. With the required functionalities, <br/>
+         and animations to make your website outstanding.<br/>
+         I develop webstes for all niches, be it Banking,<br/>
+         Architecture, Finance, and so on</p>
         </div>
-        <div className='flex flex-col gap-2'>
-          <AiOutlineAppstore className='text-xl fill-green-600'/>
+        <div className='flex flex-col gap-4 border-[3px] rounded-xl border-green-500 p-7'>
+          <AiOutlineAppstore className='text-3xl fill-green-600'/>
           <h3 className='font-bold text-xl '>WebApp Development</h3>
-          <p></p>
+          <p className='text-md font-normal text-slate-600 leading-relaxed'>I develop webApplications, with the best<br/> functionalities,
+          animations, plus amazing user experience.</p>
         </div>
         </div>
       </div>
