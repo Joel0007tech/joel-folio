3d289cb1d52d74ff43fc12706b959adba7119810
Joel <ajiolajoel@gmail.com>
2023-09-28 09:57:33 +0100

    update to my portfolio

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+      <div className='flex flex-col mt-10'>
+      <h1 className='text-[150px] text-slate-200 text-center font-semibold'>SERVICES</h1>
+        <h3 className='font-semibold text-4xl text-center relative bottom-32'>What I Do?</h3>
+      </div>
+    </div>
   )
 }
 
