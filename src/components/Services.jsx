1964db009408af0569d1e29cd68edcf77e55bd86
Joel <ajiolajoel@gmail.com>
2023-09-28 10:18:16 +0100

    update to my portfolio

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import {CgWebsite} from "react-icons/cg";
+import {AiOutlineAppstore} from "react-icons/ai";
 
 const Services = () => {
   return (
@@ -6,12 +8,16 @@ const Services = () => {
       <div className='flex flex-col mt-10'>
       <h1 className='text-[150px] text-slate-200 text-center font-semibold'>SERVICES</h1>
         <h3 className='font-semibold text-4xl text-center relative bottom-32'>What I Do?</h3>
-        <div className='flex '>
+        <div className='flex justify-center gap-10 relative bottom-12'>
         <div className='flex flex-col gap-2'>
-
+          <CgWebsite className='text-xl fill-green-600 text-gray-600'/>
+         <h3 className='font-bold text-xl '>Website Development</h3>
+         <p></p>
         </div>
         <div className='flex flex-col gap-2'>
-
+          <AiOutlineAppstore className='text-xl fill-green-600'/>
+          <h3 className='font-bold text-xl '>WebApp Development</h3>
+          <p></p>
         </div>
         </div>
       </div>
