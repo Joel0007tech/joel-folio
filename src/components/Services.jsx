251534776e21d7a339d8dbdf04840501b90fd72d
Joel <ajiolajoel@gmail.com>
2023-09-28 10:04:45 +0100

    update to my portfolio

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,6 +6,14 @@ const Services = () => {
       <div className='flex flex-col mt-10'>
       <h1 className='text-[150px] text-slate-200 text-center font-semibold'>SERVICES</h1>
         <h3 className='font-semibold text-4xl text-center relative bottom-32'>What I Do?</h3>
+        <div className='flex '>
+        <div className='flex flex-col gap-2'>
+
+        </div>
+        <div className='flex flex-col gap-2'>
+
+        </div>
+        </div>
       </div>
     </div>
   )
