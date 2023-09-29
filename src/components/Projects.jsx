d9ae0657a782610a71b1eef5c63116cbe6ff08b8
Joel <ajiolajoel@gmail.com>
2023-09-29 09:23:51 +0100

    update to my portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,6 +10,17 @@ const Projects = () => {
       <div className='flex flex-col bg-white'>
       <h1 className='text-[150px] text-slate-200 text-center font-semibold'>PROJECTS</h1>
         <h3 className='font-semibold text-4xl text-center relative bottom-32'>want to see my past works?</h3>
+        <div className='flex flex-col'>
+          <div className='flex'>
+          <img src={gpt} alt="gpt" />
+          <img src={dashboard} alt="admin-dashboard" />
+          </div>
+          
+          <div className='flex'>
+          <img src={ace} alt="ace" />
+          <img src={movie} alt="movie-task" />
+          </div>
+          </div>
         </div>
     </div>
   )
