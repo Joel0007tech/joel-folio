1629a9392fad8849191b9cee212a654a2afbafcc
Joel <ajiolajoel@gmail.com>
2023-10-02 10:16:07 +0100

    update to your portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import gpt from "../assets/gpt-3.png";
 import dashboard from "../assets/admin-dashboard.png";
-import ace from "../assets/ace project.png";
-import movie from "../assets/movie-task.png"
+import Ace from "../assets/ace project.png";
+import movie from "../assets/hoobank project.png"
 
 const Projects = ({
   background= "PROJECTS",
-  frontText ="Want to see my past works?"
+  frontText ="Want to see my past works?",
+  GPT ="chatGPT-3 Project",
+  dashbord="Dashboard Project",
+  ace ="Ace Project",
+  hoobank="Hoobank Project"
 }) => {
   return (
     <div>
@@ -18,7 +22,7 @@ const Projects = ({
           <div className='border-[5px] bg-gray-500 border-gray-500 rounded-lg p-3 flex-col gap-3'><img src={gpt} alt="gpt" width={500}
            className='border rounded-xl'/>
            <div className='flex justify-between'>
-            <p>Hello</p>
+            <p className="text-white font-normal p-3 text-lg" >{GPT}</p>
                <a href="http://" className='text-white bg border-[3px] bg-green-500 rounded-full relative bottom-4
               pl-7 pr-7 p-2 text-center text-base hover:text-black hover:bg-green-500'>
                 CLICK HERE</a>   
@@ -27,7 +31,9 @@ const Projects = ({
           <div className='border-[5px] bg-gray-500 border-gray-500 rounded-lg p-3 flex-col gap-3'><img src={dashboard} alt="admin-dashboard" width={500}
            className='border rounded-xl'/>
            <div className='flex justify-between'>
-            <p>Hello</p>
+            <p className="text-white font-normal p-3 text-lg">
+              {dashbord}
+            </p>
                <a href="http://" className='text-white bg border-[3px] bg-green-500 rounded-full relative bottom-4
               pl-7 pr-7 p-2 text-center text-base hover:text-black hover:bg-green-500'>
                 CLICK HERE</a>   
@@ -36,10 +42,10 @@ const Projects = ({
           </div>
           
           <div className='flex gap-7'>
-          <div className='border-[5px] bg-gray-500 border-gray-500 rounded-lg p-3 flex-col gap-3'><img src={ace} alt="ace" width={500}
+          <div className='border-[5px] bg-gray-500 border-gray-500 rounded-lg p-3 flex-col gap-3'><img src={Ace} alt="ace" width={500}
            className='border rounded-xl'/>
            <div className='flex justify-between'>
-            <p>Hello</p>
+            <p className="text-white font-normal p-3 text-lg">{ace}</p>
                <a href="http://" className='text-white bg border-[3px] bg-green-500 rounded-full relative bottom-4
               pl-7 pr-7 p-2 text-center text-base hover:text-black hover:bg-green-500'>
                 CLICK HERE</a>   
@@ -48,7 +54,7 @@ const Projects = ({
           <div className='border-[5px] bg-gray-500 border-gray-500 rounded-lg p-3 flex-col gap-3'><img src={movie} alt="movie-task" width={500}
            className='border rounded-xl'/>
            <div className='flex justify-between'>
-            <p>Hello</p>
+            <p className="text-white font-normal p-3 text-lg">{hoobank}</p>
                <a href="http://" className='text-white bg border-[3px] bg-green-500 rounded-full relative bottom-4
               pl-7 pr-7 p-2 text-center text-base hover:text-black hover:bg-green-500'>
                 CLICK HERE</a>   
