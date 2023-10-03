9412934e629578cf166b7c7798c196419a79d5bd
Joel <ajiolajoel@gmail.com>
2023-10-03 04:06:34 +0100

    update to my portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-const Contact = () => {
+const Contact = ({
+  background= "CONTACT",
+  frontText ="Get In Touch",
+}) => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='bg-white'>
+      <h1 className='text-[150px] text-slate-200 text-center font-semibold'>{background}</h1>
+        <h3 className='font-semibold text-4xl text-center relative bottom-32'>{frontText}</h3>
+        <div className='flex justify-center gap-6'>
+           <div className='flex flex-col'>
+
+           </div>
+           <div className='flex flex-col'>
+
+           </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
